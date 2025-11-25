2232d344fd0dc47192d85ae10a753f0d871b7451
marti.bessa <marti.bessa@estudiantat.upc.edu>
2025-11-25 10:57:50 +0100

    Implement Tag and TaskTagRelation classes #1

--- src/main/java/com/marti/domain/model/Tag.java
+++ src/main/java/com/marti/domain/model/Tag.java
@@ -1,4 +1,23 @@
 package com.marti.domain.model;
 
 public class Tag {
+    private String  id;
+    private String  name;
+    private String  user_id;
+
+    public Tag(String id, String name, String user_id) {
+        this.id = id;
+        this.name = name;
+        this.user_id = user_id;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getUser_id() {
+        return user_id;
+    }
+
 }
--- src/main/java/com/marti/domain/model/TaskTagRelation.java
+++ src/main/java/com/marti/domain/model/TaskTagRelation.java
@@ -1,4 +1,42 @@
 package com.marti.domain.model;
 
 public class TaskTagRelation {
+
+    private String taskId;
+    private String tagId;
+
+    public TaskTagRelation(String taskId, String tagId) {
+        this.taskId = taskId;
+        this.tagId = tagId;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public String getTagId() {
+        return tagId;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TaskTagRelation other = (TaskTagRelation) obj;
+        return taskId.equals(other.taskId) && tagId.equals(other.tagId);
+    }
+
+    @Override
+    public int hashCode() {
+        return taskId.hashCode() * 31 + tagId.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "TaskTagRelation{" +
+                "taskId='" + taskId + '\'' +
+                ", tagId='" + tagId + '\'' +
+                '}';
+    }
 }
